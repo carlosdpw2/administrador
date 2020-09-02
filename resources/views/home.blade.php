bbac0d262d851717c5611d55e7d7536f8c0c5df5
Carlos <alba23meira@gmail.com>
2020-09-02 14:52:28 -0400

    Subida de proyecto

---
+++ resources/views/home.blade.php
@@ -0,0 +1,147 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+
+    
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+    <div class="card">
+        <div class="card-header">
+            <h1 class="card-title">Hola Mundo</h1>
+        </div>
+        <div class="card-body">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore veritatis voluptatum natus facere earum consequuntur eaque quae corrupti laboriosam ea blanditiis vero repellendus, magni accusantium, voluptatem saepe molestiae porro debitis.
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header">
+            <h1 class="card-title">Hola Mundo</h1>
+        </div>
+        <div class="card-body">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore veritatis voluptatum natus facere earum consequuntur eaque quae corrupti laboriosam ea blanditiis vero repellendus, magni accusantium, voluptatem saepe molestiae porro debitis.
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header">
+            <h1 class="card-title">Hola Mundo</h1>
+        </div>
+        <div class="card-body">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore veritatis voluptatum natus facere earum consequuntur eaque quae corrupti laboriosam ea blanditiis vero repellendus, magni accusantium, voluptatem saepe molestiae porro debitis.
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header">
+            <h1 class="card-title">Hola Mundo</h1>
+        </div>
+        <div class="card-body">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore veritatis voluptatum natus facere earum consequuntur eaque quae corrupti laboriosam ea blanditiis vero repellendus, magni accusantium, voluptatem saepe molestiae porro debitis.
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header">
+            <h1 class="card-title">Hola Mundo</h1>
+        </div>
+        <div class="card-body">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore veritatis voluptatum natus facere earum consequuntur eaque quae corrupti laboriosam ea blanditiis vero repellendus, magni accusantium, voluptatem saepe molestiae porro debitis.
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header">
+            <h1 class="card-title">Hola Mundo</h1>
+        </div>
+        <div class="card-body">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore veritatis voluptatum natus facere earum consequuntur eaque quae corrupti laboriosam ea blanditiis vero repellendus, magni accusantium, voluptatem saepe molestiae porro debitis.
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header">
+            <h1 class="card-title">Hola Mundo</h1>
+        </div>
+        <div class="card-body">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore veritatis voluptatum natus facere earum consequuntur eaque quae corrupti laboriosam ea blanditiis vero repellendus, magni accusantium, voluptatem saepe molestiae porro debitis.
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header">
+            <h1 class="card-title">Hola Mundo</h1>
+        </div>
+        <div class="card-body">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore veritatis voluptatum natus facere earum consequuntur eaque quae corrupti laboriosam ea blanditiis vero repellendus, magni accusantium, voluptatem saepe molestiae porro debitis.
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header">
+            <h1 class="card-title">Hola Mundo</h1>
+        </div>
+        <div class="card-body">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore veritatis voluptatum natus facere earum consequuntur eaque quae corrupti laboriosam ea blanditiis vero repellendus, magni accusantium, voluptatem saepe molestiae porro debitis.
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header">
+            <h1 class="card-title">Hola Mundo</h1>
+        </div>
+        <div class="card-body">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore veritatis voluptatum natus facere earum consequuntur eaque quae corrupti laboriosam ea blanditiis vero repellendus, magni accusantium, voluptatem saepe molestiae porro debitis.
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header">
+            <h1 class="card-title">Hola Mundo</h1>
+        </div>
+        <div class="card-body">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore veritatis voluptatum natus facere earum consequuntur eaque quae corrupti laboriosam ea blanditiis vero repellendus, magni accusantium, voluptatem saepe molestiae porro debitis.
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header">
+            <h1 class="card-title">Hola Mundo</h1>
+        </div>
+        <div class="card-body">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore veritatis voluptatum natus facere earum consequuntur eaque quae corrupti laboriosam ea blanditiis vero repellendus, magni accusantium, voluptatem saepe molestiae porro debitis.
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header">
+            <h1 class="card-title">Hola Mundo</h1>
+        </div>
+        <div class="card-body">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore veritatis voluptatum natus facere earum consequuntur eaque quae corrupti laboriosam ea blanditiis vero repellendus, magni accusantium, voluptatem saepe molestiae porro debitis.
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header">
+            <h1 class="card-title">Hola Mundo</h1>
+        </div>
+        <div class="card-body">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore veritatis voluptatum natus facere earum consequuntur eaque quae corrupti laboriosam ea blanditiis vero repellendus, magni accusantium, voluptatem saepe molestiae porro debitis.
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header">
+            <h1 class="card-title">Hola Mundo</h1>
+        </div>
+        <div class="card-body">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore veritatis voluptatum natus facere earum consequuntur eaque quae corrupti laboriosam ea blanditiis vero repellendus, magni accusantium, voluptatem saepe molestiae porro debitis.
+        </div>
+    </div>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script>
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Something went wrong!',
+            footer: '<a href>Why do I have this issue?</a>'
+        })
+    </script>
+@stop
